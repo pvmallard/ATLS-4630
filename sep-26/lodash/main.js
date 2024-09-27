4fd9934c43e424076489be556a165e004539b6df
Mallory Prescott <mvprescott17@gmail.com>
2024-09-26 18:01:54 -0600

    September 26

---
+++ sep-26/lodash/main.js
@@ -0,0 +1,38 @@
+import _ from 'lodash';
+
+const myArray = ['first', 'second', 'third', 'fourth'];
+
+console.log(_.chunk(myArray,2));
+
+function scrollListener() {
+  console.log(window.scrollY);
+}
+
+const debouncedScrollListener = _.debounce(scrollListener, 250);
+
+window.addEventListener('scroll', debouncedScrollListener);
+
+// import './style.css'
+// import javascriptLogo from './javascript.svg'
+// import viteLogo from '/vite.svg'
+// import { setupCounter } from './counter.js'
+
+// document.querySelector('#app').innerHTML = `
+//   <div>
+//     <a href="https://vitejs.dev" target="_blank">
+//       <img src="${viteLogo}" class="logo" alt="Vite logo" />
+//     </a>
+//     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+//       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+//     </a>
+//     <h1>Hello Vite!</h1>
+//     <div class="card">
+//       <button id="counter" type="button"></button>
+//     </div>
+//     <p class="read-the-docs">
+//       Click on the Vite logo to learn more
+//     </p>
+//   </div>
+// `
+
+// setupCounter(document.querySelector('#counter'))
